9d2b126281a809dc667f83d464df8c7f8da953b3
mahdi <mahdimamashli1383@gmail.com>
2024-07-30 18:39:32 +0330

    feat(types): Added URLParams type and updated Context type declaration to have a newline at the end of the file.

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -5,3 +5,6 @@ export type MetadataMap = Map<MetadataKey, MetadataValue>;
 export type MetadataTarget = object;
 export type MetadataStorage = WeakMap<MetadataTarget, Map<MetadataKey, MetadataValue>>;
 export type Context = IncomingMessage & ServerResponse;
+export type URLParams<T extends Record<string, string | undefined>> = {
+  [K in keyof T]: T[K] extends string ? string : never;
+};
