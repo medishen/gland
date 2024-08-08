c30e226b2ea4dca7c89c2e1d618b68d1b66d21df
mahdi <mahdimamashli1383@gmail.com>
2024-08-08 18:29:09 +0330

    feat(types): modify Context type, add RQ and RS interfaces, and update imports

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,10 +1,17 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from 'http';
 export type MetadataKey = string | symbol;
 export type MetadataValue = any;
 export type MetadataMap = Map<MetadataKey, MetadataValue>;
 export type MetadataTarget = object;
 export type MetadataStorage = WeakMap<MetadataTarget, Map<MetadataKey, MetadataValue>>;
-export type Context = IncomingMessage & ServerResponse;
+export interface RQ extends IncomingMessage {
+  [key: string]: any;
+}
+export interface RS extends ServerResponse<IncomingMessage> {
+  [key: string]: any;
+}
+export type Context = RQ & RS ;
+
 export type URLParams<T extends Record<string, string | undefined>> = {
   [K in keyof T]: T[K] extends string ? string : never;
 };
