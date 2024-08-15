02af3a6219f502832edc0507a3fa8a0e2c3840c9
mahdi <mahdimamashli1383@gmail.com>
2024-08-15 21:04:08 +0330

    refactor(types): Updated type imports and interfaces for RQ and RS, and added WebContext to Context type

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,17 +1,19 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import { WebContext } from '../core/context';
+import { Request } from '../core/context/request';
+import { Response } from '../core/context/response';
 export type MetadataKey = string | symbol;
 export type MetadataValue = any;
 export type MetadataMap = Map<MetadataKey, MetadataValue>;
 export type MetadataTarget = object;
 export type MetadataStorage = WeakMap<MetadataTarget, Map<MetadataKey, MetadataValue>>;
-export interface RQ extends IncomingMessage {
+export interface RQ extends Request<Record<string, string>> {
   [key: string]: any;
 }
-export interface RS extends ServerResponse<IncomingMessage> {
-  [key: string]: any;
-}
-export type Context = RQ & RS ;
 
+export interface RS extends Response {
+  [key: string]: any;
+}
 export type URLParams<T extends Record<string, string | undefined>> = {
   [K in keyof T]: T[K] extends string ? string : never;
 };
+export type Context = WebContext & RQ & RS;
