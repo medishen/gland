63b74c2e02ff6270e721d1b5e51d8f5fdea19e21
mahdi <mahdimamashli1383@gmail.com>
2024-08-16 16:04:04 +0330

    refactor(types): Update type definitions for RQ and RS, add MidsFn type, and import IncomingMessage and ServerResponse from 'http'

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,19 +1,19 @@
+import { IncomingMessage, ServerResponse } from 'http';
 import { WebContext } from '../core/context';
-import { Request } from '../core/context/request';
-import { Response } from '../core/context/response';
 export type MetadataKey = string | symbol;
 export type MetadataValue = any;
 export type MetadataMap = Map<MetadataKey, MetadataValue>;
 export type MetadataTarget = object;
 export type MetadataStorage = WeakMap<MetadataTarget, Map<MetadataKey, MetadataValue>>;
-export interface RQ extends Request<Record<string, string>> {
+export interface RQ extends IncomingMessage {
   [key: string]: any;
 }
 
-export interface RS extends Response {
+export interface RS extends ServerResponse {
   [key: string]: any;
 }
-export type URLParams<T extends Record<string, string | undefined>> = {
+export type URLPrams<T extends Record<string, string | undefined>> = {
   [K in keyof T]: T[K] extends string ? string : never;
 };
 export type Context = WebContext & RQ & RS;
+export type MidsFn = (ctx: Context) => void;
