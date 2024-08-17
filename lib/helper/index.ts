b2e15768dd6fe4cafaaf4489a9cd3b52c21f5ba8
mahdi <mahdimamashli1383@gmail.com>
2024-08-17 05:06:19 +0330

    refactor(helper): rename ServerTools to Tools, move logger instance and getMethod function into ServerUtils namespace, and remove URLParser class.

--- lib/helper/index.ts
+++ lib/helper/index.ts
@@ -1,71 +1,30 @@
 import { METHODS } from 'http';
 import { Gland } from '../types/gland';
 import { Logger } from './logger';
-const logger = Logger.getInstance({ timestampFormat: 'locale', level: 'info' });
-export function getMethod(): Array<string> {
-  return (
-    METHODS &&
-    METHODS.map((m: string) => {
-      return m.toLowerCase();
-    })
-  );
-}
-export class ServerTools {
-  private static logMsg(opts: Gland.ListenOptions): string {
-    return opts.path ? `Server is running at ${opts.path}` : `Server is running at http://${opts.host}:${opts.port}`;
-  }
-  static listener(opts: Gland.ListenOptions, listeningListener?: (info: Gland.ListenOptions) => void) {
-    return () => {
-      if (listeningListener) {
-        listeningListener(opts);
-      }
-    };
-  }
-  static log(opts: Gland.ListenOptions) {
-    logger.info(ServerTools.logMsg(opts));
-  }
-}
-export class URLParser<T extends string> {
-  private url: URL;
-  private param: Record<string, string>;
-  private query: Record<string, string>;
-  private pattern: string;
-
-  constructor(urlString: string, base: string, pattern: T) {
-    this.url = new URL(urlString, base);
-    this.pattern = pattern;
-    this.param = this.extractParams();
-    this.query = this.extractQueries();
+export namespace ServerUtils {
+  const logger = Logger.getInstance({ timestampFormat: 'locale', level: 'info' });
+  export function getMethod(): Array<string> {
+    return (
+      METHODS &&
+      METHODS.map((m: string) => {
+        return m.toLowerCase();
+      })
+    );
   }
 
-  private extractParams(): Record<string, string> {
-    const pathParts = this.url.pathname.split('/').filter(Boolean);
-    const patternParts = this.pattern.split('/').filter(Boolean);
-    const params: Record<string, string> = {};
-
-    patternParts.forEach((patternPart, index) => {
-      if (patternPart.startsWith(':')) {
-        const paramName = patternPart.slice(1);
-        params[paramName] = pathParts[index];
-      }
-    });
-
-    return params;
-  }
-
-  private extractQueries(): Record<string, string> {
-    const queries: Record<string, string> = {};
-    this.url.searchParams.forEach((value, key) => {
-      queries[key] = value;
-    });
-    return queries;
-  }
-
-  get params(): Record<string, string> {
-    return this.param;
-  }
-
-  get queries(): Record<string, string> {
-    return this.query;
+  export class Tools {
+    private static logMsg(opts: Gland.ListenOptions): string {
+      return opts.path ? `Server is running at ${opts.path}` : `Server is running at http://${opts.host}:${opts.port}`;
+    }
+    static listener(opts: Gland.ListenOptions, listeningListener?: (info: Gland.ListenOptions) => void) {
+      return () => {
+        if (listeningListener) {
+          listeningListener(opts);
+        }
+      };
+    }
+    static log(opts: Gland.ListenOptions) {
+      logger.info(Tools.logMsg(opts));
+    }
   }
 }
