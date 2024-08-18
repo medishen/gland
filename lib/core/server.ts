8c6c4611932844c82c82450710b640a9f5df3e20
mahdi <mahdimamashli1383@gmail.com>
2024-08-18 19:23:48 +0330

    feat(core): add use and load method

--- lib/core/server.ts
+++ lib/core/server.ts
@@ -4,11 +4,29 @@ import { Gland } from '../types/gland';
 import { ServerUtils } from '../helper';
 import { WebContext } from './context';
 import { Router } from './router';
+import { LoadModules } from '../helper/load';
 export class WebServer extends Server implements Gland.Listener {
+  private middlewares: Gland.Middleware[] = [];
   constructor() {
     super();
   }
-  async lifecycle(req: IncomingMessage, res: ServerResponse) {
+  use(path: string | Gland.Middleware, ...handlers: Gland.Middleware[]): this {
+    if (typeof path === 'string') {
+      handlers.forEach(async (handler) => {
+        this.middlewares.push(async (ctx, next) => {
+          if (ctx.url.startsWith(path)) {
+            handler(ctx, next);
+          } else {
+            await next();
+          }
+        });
+      });
+    } else {
+      this.middlewares.push(path, ...handlers);
+    }
+    return this;
+  }
+  private async lifecycle(req: IncomingMessage, res: ServerResponse) {
     const { ctx } = new WebContext(req, res);
     const { method, path, url, base } = await Parser.Request(req);
     ctx.query = Object.fromEntries(url.searchParams.entries());
@@ -16,9 +34,8 @@ export class WebServer extends Server implements Gland.Listener {
     if (matchingRoute) {
       const { controller, handlerKey, params } = matchingRoute;
       ctx.params = params;
-
       const routeInstance = new controller();
-      await Router.run(ctx, routeInstance, method, handlerKey);
+      await Router.run(ctx, routeInstance, method, handlerKey, this.middlewares);
     }
   }
   init(opts: Gland.ListenOptions, listeningListener?: (info: Gland.ListenOptions) => void): Server {
@@ -34,6 +51,9 @@ export class WebServer extends Server implements Gland.Listener {
       this.listen(opts.port, opts.host, opts.backlog, listener);
     }
     return this;
+  }
+  async load(paths: string = './*.ts') {
+    await LoadModules.load(paths);
   }
 }
 export const g = new WebServer();
