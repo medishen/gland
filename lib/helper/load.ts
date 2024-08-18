866fc3118bcaaa4278827c981dad782969ef3e74
mahdi <mahdimamashli1383@gmail.com>
2024-08-18 19:23:21 +0330

    feat(helper): enhance module loading with pattern matching
    - Added support for file patterns in the `load` function to dynamically locate and load TypeScript files.
    - Improved directory path handling to resolve both patterns and absolute paths.
    - Implemented `find` function to recursively search for files matching specified patterns.
    - Updated `matchesPattern` function to handle wildcard patterns.
    - Added directory existence check to prevent errors for non-existent directories.
    - Refactored `containsKeyword` to efficiently identify files containing the specified keyword.

---
+++ lib/helper/load.ts
@@ -0,0 +1,106 @@
+import path from 'path';
+import { getEx } from '../core/decorators/exposed';
+import { Router } from '../core/router';
+import * as fs from 'fs';
+import { createInterface } from 'readline';
+
+export namespace LoadModules {
+  const cache: Record<string, string[]> = {};
+  const keyword = '@exposed';
+  const moduleCache: { [key: string]: any } = {};
+  /**
+   * Load modules based on the given pattern
+   * @param pt Pattern to load files
+   */
+  export async function load(pt: string) {
+    const isPattern = pt.includes('*');
+    const baseDir = isPattern ? path.dirname(pt) : path.resolve(pt);
+    const pattern = isPattern ? path.basename(pt) : '*';
+    const files = await find(baseDir, pattern);
+    console.log('files:', files);
+
+    const importPromises = files.map(async (file) => {
+      const resolvedPath = path.resolve(file);
+      if (!moduleCache[resolvedPath]) {
+        moduleCache[resolvedPath] = import(resolvedPath);
+      }
+      return moduleCache[resolvedPath];
+    });
+    // Using Promise.allSettled to ensure all promises are handled
+    await Promise.all(importPromises);
+    const exp = getEx();
+    Router.init(exp);
+  }
+
+  /**
+   * Find files based on the directory and pattern
+   * @param directory Directory to search
+   * @param pattern Pattern to match files
+   */
+  async function find(directory: string, pattern: string): Promise<string[]> {
+    if (!fs.existsSync(directory)) {
+      throw new Error(`Directory does not exist: ${directory}`);
+    }
+    if (cache[directory]) {
+      return cache[directory];
+    }
+
+    let fileList: string[] = [];
+    const items = await fs.promises.readdir(directory, { withFileTypes: true });
+
+    for (const item of items) {
+      const fullPath = path.join(directory, item.name);
+
+      if (item.isDirectory()) {
+        const subFiles = await find(fullPath, pattern);
+        fileList = fileList.concat(subFiles);
+      } else {
+        if (matchesPattern(item.name, pattern)) {
+          if (await containsKeyword(fullPath)) {
+            fileList.push(fullPath);
+          }
+        }
+      }
+    }
+
+    cache[directory] = fileList;
+    return fileList;
+  }
+  /**
+   * Check if a file name matches the given pattern
+   * @param fileName File name to check
+   * @param pattern Pattern to match
+   */
+  function matchesPattern(fileName: string, pattern: string): boolean {
+    const regex = new RegExp('^' + pattern.replace(/\*/g, '.*').replace(/\?/g, '.') + '$');
+    return regex.test(fileName);
+  }
+
+  async function containsKeyword(filePath: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      let f = false;
+      const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+      const rl = createInterface({ input: stream });
+
+      rl.on('line', (line) => {
+        if (f) {
+          return;
+        }
+        if (line.includes(keyword)) {
+          f = true;
+          rl.close();
+          stream.destroy();
+          resolve(true);
+        }
+      });
+
+      rl.on('close', () => {
+        if (!f) {
+          resolve(false);
+        }
+      });
+
+      rl.on('error', (err) => reject(err));
+    });
+  }
+}
