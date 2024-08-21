e0f8050e8fdf33dd98430ffccf533aac77a27d4c
mahdi <mahdimamashli1383@gmail.com>
2024-08-21 12:14:15 +0330

    type: add render and remove code

--- lib/types/http.d.ts
+++ lib/types/http.d.ts
@@ -6,11 +6,10 @@ declare module 'http' {
   }
 
   interface ServerResponse {
-    code(statusCode: number): this;
     clearCookie(name: string): this;
     cookie(name: string, value: string, options?: CookieOptions): this;
     redirect(url: string, statusCode?: number): void;
-    render(view: string, options?: object): void;
+    render(view: string, options?: object, callback?: (err: Error | null, html?: string) => void): void;
   }
   interface CookieOptions {
     expires?: Date;
