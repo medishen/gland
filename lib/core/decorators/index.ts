09918fb094c5c097c0c20bd6b87990f7c229ee23
mahdi <mahdimamashli1383@gmail.com>
2024-08-23 16:39:14 +0330

    refactor(decoratoes): The name of the directory was changed and both files were mixed.

--- lib/core/decorators/index.ts
+++ lib/core/decorators/index.ts
@@ -1,6 +1,30 @@
 import { ServerUtils } from '../../helper';
 import Reflect from '../../metadata/metadata';
 import { MidsFn } from '../../types/types';
+const classes: Set<any> = new Set();
+function exposed(t?: any): any {
+  const attach = (target: any = t) => {
+    classes.add(target);
+  };
+  if (typeof t === 'function') {
+    // Used as a decorator
+    attach();
+    return t;
+  } else if (t) {
+    // Used as a function
+    const classesToExpose = Array.isArray(t) ? t : [t];
+    classesToExpose.forEach(attach);
+  } else {
+    // Fallback for when expose is called with no arguments
+    return function (target: any) {
+      attach(target);
+    };
+  }
+}
+function getEx(): any[] {
+  return Array.from(classes);
+}
+export { exposed, getEx };
 export function mid(middleware: MidsFn | MidsFn[]): MethodDecorator | any {
   return (target: object, propertyKey: string | symbol, descriptor: PropertyDescriptor): void => {
     const existingMids = Reflect.get('middlewares', target.constructor.prototype, propertyKey) || [];
